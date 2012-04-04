e06daa7bbcc7e9893047fe03d9e1fbf282e3dc7e
Courtney Robinson <courtney@crlog.info>
2012-04-04 21:04:19 +0200

    Add support for Array.forEach if not support (as in some I.E versions)

--- springy.js
+++ springy.js
@@ -23,6 +23,33 @@ Copyright (c) 2010 Dennis Hotson
  OTHER DEALINGS IN THE SOFTWARE.
 */
 
+//https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/forEach
+if ( !Array.prototype.forEach ) {
+  Array.prototype.forEach = function( callback, thisArg ) {
+    var T, k;
+    if ( this == null ) {
+      throw new TypeError( " this is null or not defined" );
+    }
+    var O = Object(this);
+    var len = O.length >>> 0; // Hack to convert O.length to a UInt32
+    if ( {}.toString.call(callback) != "[object Function]" ) {
+      throw new TypeError( callback + " is not a function" );
+    }
+    if ( thisArg ) {
+      T = thisArg;
+    }
+    k = 0;
+    while( k < len ) {
+      var kValue;
+      if ( k in O ) {
+        kValue = O[ k ];
+        callback.call( T, kValue, k, O );
+      }
+      k++;
+    }
+  };
+}
+
 var Graph = function() {
 	this.nodeSet = {};
 	this.nodes = [];
