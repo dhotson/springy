3511839a5999fb6dd5d21f429e8aedbf3d9b0d37
Sreejesh Karunakaran <sree@tech-avishkar.in>
2012-10-23 15:29:30 +0530

    Added IE8 support by extending Array.prototype

--- springy.js
+++ springy.js
@@ -31,6 +31,71 @@
  */
  
 (function(Springy){
+    /*
+        Adding Array.forEach support to make it IE8 compatible
+    */
+    
+    
+    // Production steps of ECMA-262, Edition 5, 15.4.4.18
+    // Reference: http://es5.github.com/#x15.4.4.18
+    if ( !Array.prototype.forEach ) {
+     
+      Array.prototype.forEach = function forEach( callback, thisArg ) {
+     
+        var T, k;
+     
+        if ( this == null ) {
+          throw new TypeError( "this is null or not defined" );
+        }
+     
+        // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+        var O = Object(this);
+     
+        // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+        // 3. Let len be ToUint32(lenValue).
+        var len = O.length >>> 0; // Hack to convert O.length to a UInt32
+     
+        // 4. If IsCallable(callback) is false, throw a TypeError exception.
+        // See: http://es5.github.com/#x9.11
+        if ( {}.toString.call(callback) !== "[object Function]" ) {
+          throw new TypeError( callback + " is not a function" );
+        }
+     
+        // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+        if ( thisArg ) {
+          T = thisArg;
+        }
+     
+        // 6. Let k be 0
+        k = 0;
+     
+        // 7. Repeat, while k < len
+        while( k < len ) {
+     
+          var kValue;
+     
+          // a. Let Pk be ToString(k).
+          //   This is implicit for LHS operands of the in operator
+          // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+          //   This step can be combined with c
+          // c. If kPresent is true, then
+          if ( Object.prototype.hasOwnProperty.call(O, k) ) {
+     
+            // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
+            kValue = O[ k ];
+     
+            // ii. Call the Call internal method of callback with T as the this value and
+            // argument list containing kValue, k, and O.
+            callback.call( T, kValue, k, O );
+          }
+          // d. Increase k by 1.
+          k++;
+        }
+        // 8. return undefined
+      };
+    }
+
+
     /* 
         The Graph Class
     */
@@ -521,20 +586,24 @@
             var t = this;
             this.layout.start(50, function render() {
                 t.clear();
-
+                
+                t.layout.eachNode(function(node, point) {
+                    t.drawNode(node, point.p);
+                });
+                
                 t.layout.eachEdge(function(edge, spring) {
                     t.drawEdge(edge, spring.point1.p, spring.point2.p);
                 });
 
-                t.layout.eachNode(function(node, point) {
-                    t.drawNode(node, point.p);
-                });
+
             });
         },
         graphChanged: function(e) {
             this.start();
         }
     };
+    
+    
     //expose Springy
     window.Springy = Springy;
 })(window.Springy || {});
