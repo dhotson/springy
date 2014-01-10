bcd9efd2206dd9e7b0c001cb070956aaf3f6a6d7
Dennis Hotson <dennis@99designs.com>
2014-01-11 03:45:40 +1100

    Whitespace fix

--- springyui.js
+++ springyui.js
@@ -27,7 +27,7 @@ Copyright (c) 2010 Dennis Hotson
 
 jQuery.fn.springy = function(params) {
 	var graph = this.graph = params.graph || new Springy.Graph();
-        var nodeFont = "12px Verdana, sans-serif";
+	var nodeFont = "12px Verdana, sans-serif";
 	var edgeFont = "8px Verdana, sans-serif";
 	var stiffness = params.stiffness || 400.0;
 	var repulsion = params.repulsion || 400.0;
